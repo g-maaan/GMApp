abf1eea390df81c0fc55e7a7a7966582956d2e11
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-05 12:06:26 +0300

    Add files via upload

---
+++ Практические_Работы/Практическая_Работа_04/SumApp01/SumApp01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumApp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, Step = 0;
+
+            Console.WriteLine("Введите первое число");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            y = double.Parse(Console.ReadLine());
+            double z = 1;
+            while (Step < 1000)
+
+            {
+
+
+                Console.WriteLine((x + y) + ((x + y) * z));
+
+
+                Step++;
+                z++;
+            }
+            Console.WriteLine(Step);
+            Console.ReadLine();
+        }
+    }
+}
