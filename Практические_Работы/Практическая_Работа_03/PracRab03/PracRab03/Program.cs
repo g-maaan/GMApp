9487bd12e67d7a83dfeac394c944ebdb9729adef
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-05 11:36:53 +0300

    Update Program.cs

--- Практические_Работы/Практическая_Работа_03/PracRab03/PracRab03/Program.cs
+++ Практические_Работы/Практическая_Работа_03/PracRab03/PracRab03/Program.cs
@@ -10,7 +10,28 @@ namespace PracRab03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("")
+            Console.WriteLine("Имя: ");
+            string Name = (Console.ReadLine());
+            Console.WriteLine("Фамилия: ");
+            string Surname = (Console.ReadLine());
+            Console.WriteLine("Отчество: ");
+            string LName = (Console.ReadLine());
+            Console.WriteLine("Возраст: ");
+            float Years = float.Parse(Console.ReadLine());
+            Console.WriteLine("Пол: М или Ж ");
+            string Gender = (Console.ReadLine());
+            Console.WriteLine("Место учёбы: ");
+            string PlaceOfStudy = (Console.ReadLine());
+            Console.WriteLine(" Группа: ");
+            string Group = (Console.ReadLine());
+
+
+
+            Console.WriteLine(" \n***********************************\n*     * АНКЕТА ПОЛЬЗОВАТЕЛЯ *     *" + "\n***********************************\n*ФИО: " + Surname + " " + Name + " " + LName + "         *\n*Возраст: " + Years +
+            "         *\n*Пол: " + Gender + "         *\n*Место учёбы: " + PlaceOfStudy + "         *\n*Группа: " + Group + "        *\n                                  *\n***********************************");
+
+
+            Console.ReadKey();
         }
     }
 }
