c93994f04f74dcbb688e2e395642c29836b656ea
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-05 11:53:29 +0300

    Update Program.cs

--- Практические_Работы/Практическая_Работа_03/PracRab03/PracRab03/Program.cs
+++ Практические_Работы/Практическая_Работа_03/PracRab03/PracRab03/Program.cs
@@ -10,6 +10,7 @@ namespace PracRab03
     {
         static void Main(string[] args)
         {
+             Console.WriteLine("Здравствуйте, вас приветствует программа анкета. \nГрише не хватило времени сделать мне дружелюбный интерфейс.\nПоэтому, давай по делу:");
             Console.WriteLine("Имя: ");
             string Name = (Console.ReadLine());
             Console.WriteLine("Фамилия: ");
@@ -18,18 +19,35 @@ namespace PracRab03
             string LName = (Console.ReadLine());
             Console.WriteLine("Возраст: ");
             float Years = float.Parse(Console.ReadLine());
-            Console.WriteLine("Пол: М или Ж ");
-            string Gender = (Console.ReadLine());
+            
             Console.WriteLine("Место учёбы: ");
             string PlaceOfStudy = (Console.ReadLine());
             Console.WriteLine(" Группа: ");
             string Group = (Console.ReadLine());
+            Console.WriteLine("Пол: М или Ж ");
+            string Gender = (Console.ReadLine());
+            
+            switch (Gender)
+            {
 
 
-
-            Console.WriteLine(" \n***********************************\n*     * АНКЕТА ПОЛЬЗОВАТЕЛЯ *     *" + "\n***********************************\n*ФИО: " + Surname + " " + Name + " " + LName + "         *\n*Возраст: " + Years +
+                case ("М"):
+                    Console.WriteLine(" \n***********************************\n*     * АНКЕТА ПОЛЬЗОВАТЕЛЯ *     *" + "\n***********************************\n*ФИО: " + Surname + " " + Name + " " + LName + "         *\n*Возраст: " + Years +
             "         *\n*Пол: " + Gender + "         *\n*Место учёбы: " + PlaceOfStudy + "         *\n*Группа: " + Group + "        *\n                                  *\n***********************************");
 
+
+                    break;
+                case ("Ж"):
+                    Console.WriteLine(" \n***********************************\n*     * АНКЕТА ПОЛЬЗОВАТЕЛЯ *     *" + "\n***********************************\n*ФИО: " + Surname + " " + Name + " " + LName + "         *\n*Возраст: " + Years +
+            "         *\n*Пол: " + Gender + "         *\n*Место учёбы: " + PlaceOfStudy + "         *\n*Группа: " + Group + "        *\n                                  *\n***********************************");
+
+
+                    break;
+                default:
+                    Console.WriteLine("Я конечно толлерантный и все дела, но это перебор...\nПока...");
+                    break;
+            }
+           
 
             Console.ReadKey();
         }
