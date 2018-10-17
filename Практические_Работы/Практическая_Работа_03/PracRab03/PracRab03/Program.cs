4c1dc38d88d67cce61bea300d3be57db7794db9e
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-17 12:11:31 +0300

    Update Program.cs

--- Практические_Работы/Практическая_Работа_03/PracRab03/PracRab03/Program.cs
+++ Практические_Работы/Практическая_Работа_03/PracRab03/PracRab03/Program.cs
@@ -12,38 +12,38 @@ namespace PracRab03
         {
              Console.WriteLine("Здравствуйте, вас приветствует программа 'Анкета'. \nГрише не хватило времени сделать мне дружелюбный интерфейс.\nПоэтому, давай по делу:");
             Console.WriteLine("Имя: ");
-            string Name = (Console.ReadLine());
+            string Name = (Console.ReadLine());// пользователь вводит имя
             Console.WriteLine("Фамилия: ");
-            string Surname = (Console.ReadLine());
+            string Surname = (Console.ReadLine());// пользователь вводит отчество
             Console.WriteLine("Отчество: ");
-            string LName = (Console.ReadLine());
+            string LName = (Console.ReadLine());// пользователь вводит фамилию
             Console.WriteLine("Возраст: ");
-            float Years = float.Parse(Console.ReadLine());
+            float Years = float.Parse(Console.ReadLine());// пользователь вводит свой возраст
             
             Console.WriteLine("Место учёбы: ");
-            string PlaceOfStudy = (Console.ReadLine());
+            string PlaceOfStudy = (Console.ReadLine());// пользователь вводит место учёбы
             Console.WriteLine(" Группа: ");
-            string Group = (Console.ReadLine());
+            string Group = (Console.ReadLine());// пользователь вводит свою группу
             Console.WriteLine("Пол: М или Ж ");
-            string Gender = (Console.ReadLine());
+            string Gender = (Console.ReadLine());// пользователь вводит свой пол
             
             switch (Gender)
             {
 
 
-                case ("М"):
+                case ("М"):// если пользователь - мужик, то всё работает
                     Console.WriteLine(" \n***********************************\n*     * АНКЕТА ПОЛЬЗОВАТЕЛЯ *     *" + "\n***********************************\n*ФИО: " + Surname + " " + Name + " " + LName + "         *\n*Возраст: " + Years +
             "         *\n*Пол: " + Gender + "         *\n*Место учёбы: " + PlaceOfStudy + "         *\n*Группа: " + Group + "        *\n                                  *\n***********************************");
 
 
                     break;
-                case ("Ж"):
+                case ("Ж"):// и если пользователь - женщина, тоже всё работает
                     Console.WriteLine(" \n***********************************\n*     * АНКЕТА ПОЛЬЗОВАТЕЛЯ *     *" + "\n***********************************\n*ФИО: " + Surname + " " + Name + " " + LName + "         *\n*Возраст: " + Years +
             "         *\n*Пол: " + Gender + "         *\n*Место учёбы: " + PlaceOfStudy + "         *\n*Группа: " + Group + "        *\n                                  *\n***********************************");
 
 
                     break;
-                default:
+                default:// без комментариев
                     Console.WriteLine("Я конечно толлерантный и все дела, но это перебор...\nПока...");
                     break;
             }
