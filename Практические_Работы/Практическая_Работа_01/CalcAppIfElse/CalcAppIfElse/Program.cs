cd011c70e44b21ccfd2d71129b6de6782c06591a
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-17 11:37:05 +0300

    Update Program.cs

--- Практические_Работы/Практическая_Работа_01/CalcAppIfElse/CalcAppIfElse/Program.cs
+++ Практические_Работы/Практическая_Работа_01/CalcAppIfElse/CalcAppIfElse/Program.cs
@@ -12,41 +12,41 @@ namespace CalcAppIfElse
         {
             Console.WriteLine("Вас приветствует приложение Калькулятор!");
             Console.WriteLine("Как Вас зовут?");
-            string MyName = Console.ReadLine();
+            string MyName = Console.ReadLine();// вводим переменную обозначающую ваше имя
             Console.WriteLine("Здравствуйте, " + MyName
                 + "! Добро пожаловать в систему!");
 
-            string Cmd;
-            double First, Second;
+            string Cmd;// вводим переменную обозначающую знак действия
+            double First, Second;// вводим переменные обозначающие Первое и Второе число
 
             Console.WriteLine("\n" + MyName + ", программа Калькулятор может"
                  + " выполнять следующие команды:");
             Console.WriteLine(" -Plus - сложение; \n -Minus - вычитание; \n -Multiply - умножение; \n -Degree - деление.");
             Console.WriteLine("Какую кокоманды Вы хотите выполнить?");
-            Cmd = Console.ReadLine();
+            Cmd = Console.ReadLine();// позволяем пользователю выбрать действие
             Console.WriteLine("Введите первую переменную:");
-            First = double.Parse(Console.ReadLine());
+            First = double.Parse(Console.ReadLine());// позволяем пользователю ввести Первую переменную
             Console.WriteLine("Введите вторую переменную:");
-            Second = double.Parse(Console.ReadLine());
+            Second = double.Parse(Console.ReadLine());// позволяем пользователю ввести Вотрую переменную 
 
-            if (Cmd == "Plus")
+            if (Cmd == "Plus")// если пользователь решил сложить переменные
             {
                 Console.WriteLine("Итоговый результат: " + (First + Second));
             }
-            else if (Cmd == "Minus")
+            else if (Cmd == "Minus")// если пользователь решил из Первой переменной вычесть Вторую
             {
                 Console.WriteLine("Итоговый результат: " + (First - Second));
             }
-            else if (Cmd == "Multiply")
+            else if (Cmd == "Multiply")// если пользователь решил перемножить переменные
             {
                 Console.WriteLine("Итоговый результат: " + (First * Second));
             }
-            else if (Cmd == "Degree")
+            else if (Cmd == "Degree")// если пользователь решил разделить Первую переменную на Вторую
             {
                 Console.WriteLine("Итоговый результат: " + (First / Second));
             }
 
-            else
+            else // если пользователь тупой
             {
                 Console.WriteLine("Команда не входит в список программы.");
             }
