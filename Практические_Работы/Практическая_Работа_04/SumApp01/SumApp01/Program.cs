4ab23f3718fe7edee5ebf350227d32fc6ffadc59
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-17 12:19:25 +0300

    Update Program.cs

--- Практические_Работы/Практическая_Работа_04/SumApp01/SumApp01/Program.cs
+++ Практические_Работы/Практическая_Работа_04/SumApp01/SumApp01/Program.cs
@@ -10,25 +10,25 @@ namespace SumApp01
     {
         static void Main(string[] args)
         {
-            double x, y, Step = 0;
+            double x, y, Step = 0;// вводи перую переменную, вторую и переменную, обозначающую кол-во повторений действия
 
             Console.WriteLine("Введите первое число");
-            x = double.Parse(Console.ReadLine());
+            x = double.Parse(Console.ReadLine());// пользователь вводит первое число
             Console.WriteLine("Введите второе число");
-            y = double.Parse(Console.ReadLine());
-            double z = 1;
+            y = double.Parse(Console.ReadLine());// пользователь вводит второе число
+            double z = 1;// вводится переменная, обозначающая прибавление действий
             while (Step < 1000)
 
             {
 
 
-                Console.WriteLine((x + y) + ((x + y) * z));
+                Console.WriteLine((x + y) + ((x + y) * z));// таким образом, каждый раз z возрастает на 1, и к каждой предыдущей сумме двух переменных прибавляется следующая
 
 
                 Step++;
                 z++;
             }
-            Console.WriteLine(Step);
+            Console.WriteLine(Step);// выводится кол-во повторений
             Console.ReadLine();
         }
     }
