d8a544d357b7831d54a80c90e5a904d374ec7f67
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-17 10:25:20 +0300

    Add files via upload

---
+++ Практические_Работы/Практическая_Работа_08/PracRab08/PracRab08/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracRab08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int[,] numbers = new int[x, y];
+            {
+
+                for (; ; x++)
+                {
+                    for (; ; y++)
+                    {
+                        numbers[x, y] = rnd.Next(-1000, 1000);
+                        Console.Write("({1}) -> {0}\t", numbers[x, y]);
+                    }
+                    Console.WriteLine();
+                }
+            
+            Console.ReadKey();
+        }
+    }
+}
