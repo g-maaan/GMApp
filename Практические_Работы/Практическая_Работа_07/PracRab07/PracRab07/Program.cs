b116bf2513893c77b61546e5b4fe5a7ae2f4521a
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-18 13:30:37 +0300

    Update Program.cs

--- Практические_Работы/Практическая_Работа_07/PracRab07/PracRab07/Program.cs
+++ Практические_Работы/Практическая_Работа_07/PracRab07/PracRab07/Program.cs
@@ -10,54 +10,87 @@ namespace PracRab07
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Здравствуйте, Вас приветствует Calculator(Nedruzhelubniy interface edition)");
+
             Console.WriteLine("Введите Первое и Второе число:");
-            int[] numbers = new int[2] {int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())};
+
+            double[] numbers = new double[2] { double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()) };
+
             Console.WriteLine("Их можно сложить(+), из Первого можно вычесть Второе(-), Первое можно поделить на Второе(/) и их можно перемножить(*).");
+
             Console.WriteLine("Выберите действие:");
+
             string Cmd = Console.ReadLine();
+
+
 
             switch (Cmd)
 
+
+
             {
+
+
 
                 case "+":
 
-                    Console.WriteLine(numbers[0]+ numbers[1]);
+
+
+                    Console.WriteLine(numbers[0] + numbers[1]);
+
+
 
                     break;
+
+
 
                 case "-":
 
+
+
                     Console.WriteLine(numbers[0] - numbers[1]);
 
+
+
                     break;
+
+
 
                 case "*":
 
+
+
                     Console.WriteLine(numbers[0] * numbers[1]);
 
+
+
                     break;
+
+
 
                 case "/":
 
+
+
                     Console.WriteLine(numbers[0] / numbers[1]);
 
+
+
                     break;
+
+
 
                 default:
 
+
+
                     Console.WriteLine("ERROR");
+
+
 
                     break;
 
-            }
-
-
-
-
-
-            Console.ReadKey();
 
 
 
