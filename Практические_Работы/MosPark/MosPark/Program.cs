d04f5e8280b99c7b596e2ff1aac2056ff4080ffc
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-10-24 12:12:44 +0300

    Add files via upload

---
+++ Практические_Работы/MosPark/MosPark/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosPark
+{
+
+    class Automobile
+    {
+        public string Marka = Console.ReadLine();
+        public string Model = Console.ReadLine();
+        public string color = Console.ReadLine();
+
+    }
+    class Parking
+    {
+        public int Number = int.Parse(Console.ReadLine());
+        public DateTime currentDate = DateTime.Now;
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine(currentDate);
+
+            Console.WriteLine(Marka);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
