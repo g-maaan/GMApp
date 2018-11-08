1784bd32358f7a0bea097e1b09c621f3926bc399
g-maaan <43873501+g-maaan@users.noreply.github.com>
2018-11-08 13:25:03 +0300

    Update Program.cs

--- Практические_Работы/Практическая_Работа_08/PracRab08/PracRab08/Program.cs
+++ Практические_Работы/Практическая_Работа_08/PracRab08/PracRab08/Program.cs
@@ -10,23 +10,63 @@ namespace PracRab08
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int x = int.Parse(Console.ReadLine());
-            int y = int.Parse(Console.ReadLine());
-            int[,] numbers = new int[x, y];
+           
+            static void Main(string[] args)
             {
+            Console.OutputEncoding = Encoding.UTF8;
+            int stroka = GetCountRows();
+                int stolbec = GetStolbec();
 
-                for (; ; x++)
+                int[,] array = new int[stroka, stolbec];
+
+                array = InputArray(stroka, stolbec);
+                ShowArray(array);
+
+                Console.ReadKey();
+            }
+
+            static int GetCountRows()
+            {
+                Console.Write("Количество строк = ");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+            static int GetStolbec()
+            {
+                Console.Write("\nКоличество столбцов = ");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+            static int[,] InputArray(int stroka, int stolbec)
+            {
+                int[,] array = new int[stroka, stolbec];
+                Console.WriteLine();
+                for (int i = 0; i < stroka; i++)
                 {
-                    for (; ; y++)
+                    for (int j = 0; j < stolbec; j++)
                     {
-                        numbers[x, y] = rnd.Next(-1000, 1000);
-                        Console.Write("({1}) -> {0}\t", numbers[x, y]);
+                        Console.Write("Введите число ->  ");
+                        array[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+               }
+
+                return array;
+            }
+
+            static void ShowArray(int[,] array)
+            {
+                Console.WriteLine("\n" +
+                    "А вот и твой массивчик");
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write("{0}\t", array[i, j]);
                     }
                     Console.WriteLine();
+
                 }
-            
-            Console.ReadKey();
+            }
         }
     }
 }
